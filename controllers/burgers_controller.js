12d3803d0248280730e2f6a49ed3f0cae00a5aec
Casey Overton <overtonc1@gmail.com>
2020-12-22 12:47:16 -0500

    added route instead of app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,23 +1,24 @@
 const path = require("path");
 burger = require("../models/burger");
+var router = express.Router();
 
 module.exports = function(app) {
-	app.get("/", function(req, res) {
+	router.get("/", function(req, res) {
 		burger.getBurgers(function(burgers) {
 			res.render(path.join(__dirname, "../views/index"), {burgers:burgers});
 		});
 	});
-	// app.get("/burgers", function(req, res) {
-	// 	burger.getBurgers(function(burgers) {
-	// 		res.json(burgers);
-	// 	});
-	// });
-	app.post("/burger/add", function(req, res) {
+	router.get("/burgers", function(req, res) {
+		burger.getBurgers(function(burgers) {
+			res.json(burgers);
+		});
+	});
+	router.post("/burger/add", function(req, res) {
 		burger.addBurger(req.body.burger_name, function(burgers) {
 			res.redirect('/');
 		});
 	});
-	app.post("/burger/eat/:burger_id", function(req, res) {
+	router.post("/burger/eat/:burger_id", function(req, res) {
 		burger.devourBurger(req.params.burger_id, function(burgers) {
 			burger.getBurgers(function(burgers) {
 				res.json(burgers);
